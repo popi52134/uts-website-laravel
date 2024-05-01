5bb73592fb2a00b61e5aef30197d91eb8be22285
popi52134 <email@anda.com>
2024-05-01 08:16:38 +0700

    soal no.4

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -10,6 +10,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <body>
+    {{-- no4 --}}
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">UTS Laravel Popi</a>
